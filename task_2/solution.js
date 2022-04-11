67d6944db41c4ee057025907704d7775ad82a251
TRisha125 <Irina75t@mail.ru>
2022-04-11 23:32:13 +1000

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,6 +6,13 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
+    let shippingSum;
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }  
+    if (productsSum > 0 && productsSum < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
@@ -29,6 +36,11 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
+    let discountSum
+    if (productsSum >= discountMinSum) {
+        discountSum = discountPart / 100 * productsSum;
+    } else (discountSum = 0);
+ 
 
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
@@ -46,6 +58,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
     // создайте переменную totalSum
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
@@ -53,8 +68,10 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
-
+    totalSum = totalSum + shippingSum;
     // создайте переменную freeShipping
+    let freeShipping;
+    (shippingSum == 0) ? freeShipping = true : freeShipping = false;
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
